7b6e3ff1df92f28ecbd9bcfe48a4ac550d9c0540
Eli <elipup@gmail.com>
2015-12-17 14:33:46 -0500

    Add nginx and neatly pack all the operations into ecommerce.sh

---
+++ ecommerce.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+setup_ecommerce() {
+    set_ecommerce_info
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        return $RET
+    fi
+    
+    log_choice install_postgresql 9.3
+    log_choice drop_cluster 9.3 main auto
+    
+    provision_ecom_cluster
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        msgbox "Error while provisioning a postgres eCommerce cluster."
+        return $RET
+    fi
+    
+    configure_postfix
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        msgbox "Error while configuring postfix"
+        return $RET
+    fi
+    
+    install_nginx
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        msgbox "Error while installing nginx"
+	   return $RET
+    fi
+    prep_nginx
+    configure_nginx_ecom
+    
+    configure_php
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        msgbox "Error while configuring php."
+        return $RET
+    fi
+    
+    configure_mongo
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        msgbox "Error while configuring mongo."
+        return $RET
+    fi
+}
